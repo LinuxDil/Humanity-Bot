29e03d286ee25ce6d0e0f860c06c36944c9dcb29
LinuxDil <ari.standart@gmail.com>
2025-04-16 17:08:46 +0000

    Update humanity.py

--- humanity.py
+++ humanity.py
@@ -1,144 +1,99 @@
 import time
-import threading
-import requests
+import sys
 from web3 import Web3
 from colorama import init, Fore, Style
-import sys
 
-# Initialize colorama
+# Init colorama
 init(autoreset=True)
 
-# Header message
+# Header info
 def display_header():
     print(Fore.CYAN + Style.BRIGHT + "===============================")
     print(Fore.YELLOW + Style.BRIGHT + "Auto Daily Claim $RWT Humanity Protocol")
     print(Fore.CYAN + Style.BRIGHT + "Bot created by: Bang Alex")
     print(Fore.YELLOW + Style.BRIGHT + "Join Our Channel at: " + Fore.GREEN + "https://t.me/airdropseeker_official")
-
     print(Fore.CYAN + Style.BRIGHT + "===============================\n")
 
-# Connect to the blockchain network
+# RPC dan koneksi Web3
 rpc_url = 'https://rpc.testnet.humanity.org'
 web3 = Web3(Web3.HTTPProvider(rpc_url))
 
-# Check if connected to the network
-if web3.is_connected():
-    print(Fore.GREEN + "Connected to Humanity Protocol")
+if not web3.is_connected():
+    print(Fore.RED + "❌ Gagal konek ke Humanity Protocol.")
+    sys.exit(1)
 else:
-    print(Fore.RED + "Connection failed.")
-    sys.exit(1)  # Exit if connection fails
+    print(Fore.GREEN + "✅ Terkoneksi ke Humanity Protocol.\n")
 
-# Smart contract address and ABI
+# Alamat dan ABI kontrak
 contract_address = '0xa18f6FCB2Fd4884436d10610E69DB7BFa1bFe8C7'
-contract_abi = [{"inputs":[],"name":"AccessControlBadConfirmation","type":"error"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"bytes32","name":"neededRole","type":"bytes32"}],"name":"AccessControlUnauthorizedAccount","type":"error"},{"inputs":[],"name":"InvalidInitialization","type":"error"},{"inputs":[],"name":"NotInitializing","type":"error"},{"anonymous":False,"inputs":[{"indexed":False,"internalType":"uint64","name":"version","type":"uint64"}],"name":"Initialized","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"from","type":"address"},{"indexed":True,"internalType":"address","name":"to","type":"address"},{"indexed":False,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":False,"internalType":"bool","name":"bufferSafe","type":"bool"}],"name":"ReferralRewardBuffered","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"user","type":"address"},{"indexed":True,"internalType":"enum IRewards.RewardType","name":"rewardType","type":"uint8"},{"indexed":False,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"RewardClaimed","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":True,"internalType":"bytes32","name":"previousAdminRole","type":"bytes32"},{"indexed":True,"internalType":"bytes32","name":"newAdminRole","type":"bytes32"}],"name":"RoleAdminChanged","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":True,"internalType":"address","name":"account","type":"address"},{"indexed":True,"internalType":"address","name":"sender","type":"address"}],"name":"RoleGranted","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":True,"internalType":"address","name":"account","type":"address"},{"indexed":True,"internalType":"address","name":"sender","type":"address"}],"name":"RoleRevoked","type":"event"},{"inputs":[],"name":"DEFAULT_ADMIN_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimBuffer","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"claimReward","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"currentEpoch","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cycleStartTimestamp","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleAdmin","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"grantRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"hasRole","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"vcContract","type":"address"},{"internalType":"address","name":"tkn","type":"address"}],"name":"init","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"callerConfirmation","type":"address"}],"name":"renounceRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"revokeRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"startTimestamp","type":"uint256"}],"name":"start","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"stop","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"userBuffer","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"epochID","type":"uint256"}],"name":"userClaimStatus","outputs":[{"components":[{"internalType":"uint256","name":"buffer","type":"uint256"},{"internalType":"bool","name":"claimStatus","type":"bool"}],"internalType":"struct IRewards.UserClaim","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"userGenesisClaimStatus","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]
+contract_abi = contract_abi = [{"inputs":[],"name":"AccessControlBadConfirmation","type":"error"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"bytes32","name":"neededRole","type":"bytes32"}],"name":"AccessControlUnauthorizedAccount","type":"error"},{"inputs":[],"name":"InvalidInitialization","type":"error"},{"inputs":[],"name":"NotInitializing","type":"error"},{"anonymous":False,"inputs":[{"indexed":False,"internalType":"uint64","name":"version","type":"uint64"}],"name":"Initialized","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"from","type":"address"},{"indexed":True,"internalType":"address","name":"to","type":"address"},{"indexed":False,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":False,"internalType":"bool","name":"bufferSafe","type":"bool"}],"name":"ReferralRewardBuffered","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"user","type":"address"},{"indexed":True,"internalType":"enum IRewards.RewardType","name":"rewardType","type":"uint8"},{"indexed":False,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"RewardClaimed","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":True,"internalType":"bytes32","name":"previousAdminRole","type":"bytes32"},{"indexed":True,"internalType":"bytes32","name":"newAdminRole","type":"bytes32"}],"name":"RoleAdminChanged","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":True,"internalType":"address","name":"account","type":"address"},{"indexed":True,"internalType":"address","name":"sender","type":"address"}],"name":"RoleGranted","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":True,"internalType":"address","name":"account","type":"address"},{"indexed":True,"internalType":"address","name":"sender","type":"address"}],"name":"RoleRevoked","type":"event"},{"inputs":[],"name":"DEFAULT_ADMIN_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimBuffer","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"claimReward","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"currentEpoch","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cycleStartTimestamp","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleAdmin","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"grantRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"hasRole","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"vcContract","type":"address"},{"internalType":"address","name":"tkn","type":"address"}],"name":"init","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"callerConfirmation","type":"address"}],"name":"renounceRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"revokeRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"startTimestamp","type":"uint256"}],"name":"start","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"stop","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"userBuffer","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"epochID","type":"uint256"}],"name":"userClaimStatus","outputs":[{"components":[{"internalType":"uint256","name":"buffer","type":"uint256"},{"internalType":"bool","name":"claimStatus","type":"bool"}],"internalType":"struct IRewards.UserClaim","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"userGenesisClaimStatus","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]
+ # <--- paste ABI kamu di sini, sama seperti versi awal kamu
 
-# Load the contract
+# Load kontrak
 contract = web3.eth.contract(address=Web3.to_checksum_address(contract_address), abi=contract_abi)
 
-# Function to load private keys from a text file
+# Fungsi untuk load private keys dari file
 def load_private_keys(file_path):
     with open(file_path, 'r') as file:
-        private_keys = [line.strip() for line in file if line.strip()]
-    return private_keys
+        return [line.strip() for line in file if line.strip()]
 
-# Function to claim rewards
+# Fungsi klaim reward
 def claim_rewards(private_key):
     try:
-        # Derive the sender's address from the private key
         account = web3.eth.account.from_key(private_key)
-        sender_address = account.address
+        sender = account.address
 
-        # Check if the genesis reward has already been claimed
-        genesis_reward_claimed = contract.functions.userGenesisClaimStatus(sender_address).call()
-
-        # Get the current epoch from the contract
+        genesis_claimed = contract.functions.userGenesisClaimStatus(sender).call()
         current_epoch = contract.functions.currentEpoch().call()
+        buffer_amount, claimed = contract.functions.userClaimStatus(sender, current_epoch).call()
 
-        # Check if the reward has already been claimed for the current epoch
-        reward_claim_status = contract.functions.userClaimStatus(sender_address, current_epoch).call()
-
-        # reward_claim_status is a tuple: (buffer, claimStatus)
-        buffer_amount, claim_status = reward_claim_status
-        
-        # If the genesis reward is claimed
-        if genesis_reward_claimed:
-            # If userClaimStatus is False, proceed to claim the reward
-            if not claim_status:
-                print(Fore.GREEN + f"Proceeding to claim reward for address: {sender_address} (Genesis reward claimed).")
-                proceed_to_claim(sender_address, private_key)
-            else:
-                print(Fore.YELLOW + f"Reward already claimed for address: {sender_address} in epoch {current_epoch}. Skipping.")
-
-        # If the genesis reward is not claimed
+        if not claimed:
+            print(Fore.GREEN + f"🔁 Klaim reward untuk {sender} (Genesis claimed: {genesis_claimed})")
+            proceed_to_claim(sender, private_key)
         else:
-            print(Fore.GREEN + f"Proceeding to claim reward for address: {sender_address} (Genesis reward not claimed).")
-            proceed_to_claim(sender_address, private_key)
-
+            print(Fore.YELLOW + f"✅ Reward sudah diklaim untuk {sender} (epoch {current_epoch})")
     except Exception as e:
-        error_message = str(e)
-
-        # Check for specific error: "Rewards: user not registered"
-        if "Rewards: user not registered" in error_message:
-            print(Fore.RED + f"Error: User {sender_address} is not registered.")
+        if "user not registered" in str(e):
+            print(Fore.RED + f"❌ User belum terdaftar: {sender}")
         else:
-            print(Fore.RED + f"Error claiming reward for {sender_address}: {error_message}")
-
+            print(Fore.RED + f"⚠️ Error saat klaim untuk {sender}: {e}")
+# claim
 def proceed_to_claim(sender_address, private_key):
     try:
-        # Estimate gas limit for the claimReward transaction
-        gas_amount = contract.functions.claimReward().estimate_gas({
-            'chainId': web3.eth.chain_id,
-            'from': sender_address,
-            'gasPrice': web3.eth.gas_price,
-            'nonce': web3.eth.get_transaction_count(sender_address)
-        })
+        gas_price = web3.to_wei(5, 'gwei')
+        nonce = web3.eth.get_transaction_count(sender_address)
+        gas_limit = contract.functions.claimReward().estimate_gas({'from': sender_address})
 
-        # Build the transaction to call the 'claimReward' function
         transaction = contract.functions.claimReward().build_transaction({
             'chainId': web3.eth.chain_id,
             'from': sender_address,
-            'gas': gas_amount,
-            'gasPrice': web3.eth.gas_price,
-            'nonce': web3.eth.get_transaction_count(sender_address)
+            'gas': gas_limit,
+            'gasPrice': gas_price,
+            'nonce': nonce
         })
 
-        # Sign the transaction with the private key
-        signed_txn = web3.eth.account.sign_transaction(transaction, private_key=private_key)
+        signed_tx = web3.eth.account.sign_transaction(transaction, private_key)
+        tx_hash = web3.eth.send_raw_transaction(signed_tx['rawTransaction'])
 
-        # Send the transaction
-        tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        print(Fore.CYAN + f"⏳ Menunggu konfirmasi untuk transaksi: {web3.to_hex(tx_hash)}")
+        receipt = web3.eth.wait_for_transaction_receipt(tx_hash, timeout=180)
+        print(Fore.GREEN + f"✅ Transaksi sukses untuk {sender_address}, hash: {web3.to_hex(tx_hash)}")
 
-        # Wait for the transaction receipt
-        tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
-
-        print(Fore.GREEN + f"Transaction successful for {sender_address} with hash: {web3.to_hex(tx_hash)}")
-    
     except Exception as e:
-        print(Fore.RED + f"Error processing claim for {sender_address}: {str(e)}")
-
-# Function to claim faucet tokens
-
-# Function to perform periodic task (including faucet claim)
-
-            
-            # Wait 1 minute before claiming for the next wallet
+        print(Fore.RED + f"⚠️ Gagal memproses klaim untuk {sender_address}: {str(e)}")
 
 
-        # Optionally add other periodic tasks here, if needed
-
-# Main execution: display header, load private keys, and claim rewards for each
+# Main
 if __name__ == "__main__":
     display_header()
+    try:
+        while True:
+            keys = load_private_keys('private_keys.txt')
+            for pk in keys:
+                claim_rewards(pk)
 
-    # Start the periodic task in a separate thread
+            print(Fore.CYAN + "\n⏳ Menunggu 6 jam sebelum klaim berikutnya...\n")
+            time.sleep(6 * 60 * 60)
 
-    
-    # Infinite loop to run the process every 6 hours (claiming rewards)
-    while True:
-        private_keys = load_private_keys('private_keys.txt')
-        for private_key in private_keys:
-            claim_rewards(private_key)
-
-        # Wait for 6 hours (1 * 60 * 60 seconds)
-        print(Fore.CYAN + "Waiting for 6 hours before the next run...")
-        time.sleep(6*60*60)  # For testing purposes, you may want to reduce this time
+    except KeyboardInterrupt:
+        print(Fore.RED + "\n⛔ Dihentikan manual. Sampai jumpa!")
